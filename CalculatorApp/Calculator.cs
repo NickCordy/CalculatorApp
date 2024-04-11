9aa17da54e9f8093ebd83eb54c37c9b698485dcd
Nick Cordy <w0464869@campus.nscc.ca>
2024-04-10 22:22:04 -0300

    Add project files.

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Calculator
+    {
+        public enum Operation {
+            NONE, ADD, SUBTRACT, MULTIPLY, DIVIDE,
+        }
+        public Operation operation { get; set; } = Operation.NONE;
+        public string firstNumber { get; set; } = string.Empty;
+        public string secondNumber { get; set; } = string.Empty;
+
+        public void AppendToNumber(string digit)
+        {
+            if (operation == Operation.NONE)
+            {
+                if (digit == "." && firstNumber.Contains("."))
+                {
+                    return;
+                }
+
+                firstNumber += digit;
+            }
+            else
+            {
+                if (digit == "." && secondNumber.Contains("."))
+                {
+                    return;
+                }
+
+                secondNumber += digit;
+            }
+        }
+
+        public void Calculate()
+        {
+            double result = 0;
+            string resultStr;
+
+            if (secondNumber != string.Empty)
+            {
+                switch (operation)
+                {
+                    case Operation.ADD:
+                        result = Add();
+                        break;
+                    case Operation.SUBTRACT:
+                        result = Subtract();
+                        break;
+                    case Operation.MULTIPLY:
+                        result = Multiply();
+                        break;
+                    case Operation.DIVIDE:
+                        result = Divide();
+                        break;
+                }
+
+                resultStr = result.ToString();
+                firstNumber = resultStr;
+                secondNumber = string.Empty;
+                operation = Operation.NONE;
+            }
+        }
+
+        public void Clear()
+        {
+            operation = Operation.NONE;
+            firstNumber = string.Empty;
+            secondNumber = string.Empty;
+        }
+
+        public void ClearEntry()
+        {
+            if (operation == Operation.NONE)
+            {
+                firstNumber = string.Empty;
+            }
+            else
+            {
+                secondNumber = string.Empty;
+            }
+        }
+
+        public void Percent()
+        {
+            if (secondNumber != string.Empty)
+            {
+                secondNumber = (double.Parse(secondNumber) / 100).ToString();
+            }
+        }
+
+        public double Add()
+        {
+            return double.Parse(firstNumber) + double.Parse(secondNumber);
+        }
+
+        public double Subtract()
+        {
+            return double.Parse(firstNumber) - double.Parse(secondNumber);
+        }
+
+        public double Multiply()
+        {
+            return double.Parse(firstNumber) * double.Parse(secondNumber);
+        }
+
+        public double Divide()
+        {
+            return double.Parse(firstNumber) / double.Parse(secondNumber);
+        }
+
+        public void Square()
+        {
+            if (operation == Operation.NONE)
+            {
+                firstNumber = (double.Parse(firstNumber) * double.Parse(firstNumber)).ToString();
+            }
+            else
+            {
+                secondNumber = (double.Parse(secondNumber) * double.Parse(secondNumber)).ToString();
+            }
+        }
+
+        public void SquareRoot()
+        {
+            if (firstNumber != string.Empty)
+            {
+                if (operation == Operation.NONE)
+                {
+                    firstNumber = Convert.ToString(Math.Sqrt(Convert.ToDouble(firstNumber)));
+                }
+                else
+                {
+                    secondNumber = Convert.ToString(Math.Sqrt(Convert.ToDouble(secondNumber)));
+                }
+            }
+        }
+
+        public void Negate()
+        {
+            if (operation == Operation.NONE && firstNumber != string.Empty)
+            {
+                firstNumber = (-double.Parse(firstNumber)).ToString();
+            }
+            else if (secondNumber != string.Empty)
+            {
+                secondNumber = (-double.Parse(secondNumber)).ToString();
+            }
+        }
+
+        public void Reciprocal()
+        {
+            if (firstNumber != string.Empty)
+            {
+                if (operation == Operation.NONE)
+                {
+                    firstNumber = Convert.ToString(1 / Convert.ToDouble(firstNumber));
+                }
+                else
+                {
+                    secondNumber = Convert.ToString(1 / Convert.ToDouble(secondNumber));
+                }
+            }
+        }
+
+        public void Backspace()
+        {
+            if (operation == Operation.NONE && secondNumber == string.Empty)
+            {
+                firstNumber = firstNumber.Remove(firstNumber.Length - 1, 1);
+
+                if (firstNumber == "-")
+                {
+                    firstNumber = string.Empty;
+                }
+            }
+            else if (secondNumber != string.Empty)
+            {
+                secondNumber = secondNumber.Remove(secondNumber.Length - 1, 1);
+
+                if (secondNumber == "-")
+                {
+                    secondNumber = string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ CalculatorApp/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static CalculatorApp.Calculator;
+
+namespace CalculatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new();
+        }
+
+        private void UpdateDisplay()
+        {
+            string op = string.Empty;
+
+            switch (calculator.operation)
+            {
+                case Operation.NONE:
+                    op = string.Empty;
+                    break;
+                case Operation.DIVIDE:
+                    op = " / ";
+                    break;
+                case Operation.MULTIPLY:
+                    op = " x ";
+                    break;
+                case Operation.SUBTRACT:
+                    op = " - ";
+                    break;
+                case Operation.ADD:
+                    op = " + ";
+                    break;
+            }
+
+            txtDisplay.Text = calculator.firstNumber + op + calculator.secondNumber;
+        }
+
+        private void btnPercent_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Percent();
+            UpdateDisplay();
+        }
+
+        private void btnClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.ClearEntry();
+            UpdateDisplay();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Clear();
+            UpdateDisplay();
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Backspace();
+            UpdateDisplay();
+        }
+
+        private void btnReciprocal_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Reciprocal();
+            UpdateDisplay();
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Square();
+            UpdateDisplay();
+        }
+
+        private void btnSquareRoot_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.SquareRoot();
+            UpdateDisplay();
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.operation = Operation.DIVIDE;
+            UpdateDisplay();
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.operation = Operation.MULTIPLY;
+            UpdateDisplay();
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.operation = Operation.SUBTRACT;
+            UpdateDisplay();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.operation = Operation.ADD;
+            UpdateDisplay();
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("0");
+            UpdateDisplay();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("1");
+            UpdateDisplay();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("2");
+            UpdateDisplay();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("3");
+            UpdateDisplay();
+        }
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("4");
+            UpdateDisplay();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("5");
+            UpdateDisplay();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("6");
+            UpdateDisplay();
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("7");
+            UpdateDisplay();
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("8");
+            UpdateDisplay();
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber("9");
+            UpdateDisplay();
+        }
+
+        private void btnNegate_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Negate();
+            UpdateDisplay();
+        }
+
+        private void btnDecimalPoint_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.AppendToNumber(".");
+            UpdateDisplay();
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Calculate();
+            UpdateDisplay();
+        }
+    }
+}
